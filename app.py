a6f286d940e1cd9f664bbf4ae76303dfd278f494
sobralfabio454-bit <sobralfabio454@gmail.com>
2026-02-14 14:10:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ if api_key:
         genai.configure(api_key=api_key)
         
         # AJUSTE AQUI: Tentando o modelo mais atualizado disponível
-        # O sistema testará o Gemini 3 Flash que é o padrão estável atual
-        model = genai.GenerativeModel('gemini-3-flash')
-
+        # O sistema testará o Gemini 1.5 Flash que é o padrão estável atual
+       model = genai.GenerativeModel('models/gemini-1.5-flash-latest')
         uploaded_file = st.file_uploader("📸 Tire uma foto ou suba a imagem do Piso/Porcelanato", type=["jpg", "jpeg", "png"])
 
         if uploaded_file is not None:
